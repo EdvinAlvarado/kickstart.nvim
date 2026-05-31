e6f9943565dae2cb3a967e9fb1c7af152d2f47c1
EdvinAlvarado <ealvarado301@gmail.com>
2026-05-31 05:40:00 -0500

    formatting

--- init.lua
+++ init.lua
@@ -992,11 +992,11 @@ require('lazy').setup({
     'ziglang/zig.vim',
     event = { 'BufRead *.zig' },
     ft = 'zig',
-    config = function()
-      vim.keymap.set('n', '<c-b>', '<cmd>w<cr>' .. '<cmd>compiler zig_build<cr>')
-      vim.keymap.set('n', '<c-t>', '<cmd>w<cr>' .. '<cmd>compiler zig_test<cr>')
-      vim.keymap.set('n', '<c-r>', '<cmd>w<cr>' .. '<cmd>compiler zig_build_exe<cr>')
-    end,
+    keys = {
+      vim.keymap.set('n', '<c-b>', '<cmd>w<cr>' .. '<cmd>compiler zig_build<cr>'),
+      vim.keymap.set('n', '<c-t>', '<cmd>w<cr>' .. '<cmd>compiler zig_test<cr>'),
+      vim.keymap.set('n', '<c-r>', '<cmd>w<cr>' .. '<cmd>compiler zig_build_exe<cr>'),
+    },
   },
   {
     'LhKipp/nvim-nu',
