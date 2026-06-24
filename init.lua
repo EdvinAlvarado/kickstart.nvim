b5de4a90d31c74c4a6fd21aa645d5b8f20d8780f
EdvinAlvarado <ealvarado301@gmail.com>
2026-06-23 21:20:47 -0500

    misc

--- init.lua
+++ init.lua
@@ -1113,18 +1113,18 @@ require('lazy').setup({
       vim.keymap.set('n', '<LocalLeader>c', '<cmd>LlamaChat<cr>', { noremap = true, silent = true }),
       vim.keymap.set('v', '<LocalLeader>c', '<cmd>LlamaChat<cr>', { noremap = true, silent = true }),
     },
-    {
-      'https://git.sr.ht/~swaits/zellij-nav.nvim',
-      lazy = true,
-      event = 'VeryLazy',
-      keys = {
-        { '<c-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left' } },
-        { '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
-        { '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
-        { '<c-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right' } },
-      },
-      opts = {},
+  },
+  {
+    'https://git.sr.ht/~swaits/zellij-nav.nvim',
+    lazy = true,
+    event = 'VeryLazy',
+    keys = {
+      { '<c-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left' } },
+      { '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
+      { '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
+      { '<c-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right' } },
     },
+    opts = {},
   },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
