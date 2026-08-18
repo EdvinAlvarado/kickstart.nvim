8cbabca2aa1ded4b7a102e8b3a019881a42c3a72
EdvinAlvarado <ealvarado301@gmail.com>
2026-08-18 00:12:54 -0500

    fix overlapping maps

--- after/ftplugin/haskell.lua
+++ after/ftplugin/haskell.lua
@@ -6,7 +6,7 @@ local opts = { noremap = true, silent = true, buffer = bufnr }
 -- so auto-refresh (see advanced configuration) is enabled by default
 vim.keymap.set('n', '<space>cl', vim.lsp.codelens.run, opts)
 -- Hoogle search for the type signature of the definition under the cursor
-vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
+vim.keymap.set('n', '<space>hh', ht.hoogle.hoogle_signature, opts)
 -- Evaluate all code snippets
 vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
 -- Toggle a GHCi repl for the current package
