01218d252b9cb497e28e679f7f524b0ca4c85469
EdvinAlvarado <ealvarado301@gmail.com>
2026-08-18 00:13:24 -0500

    odin

--- after/ftplugin/odin.lua
+++ after/ftplugin/odin.lua
@@ -1,3 +1,3 @@
 vim.keymap.set('n', '<c-b>', '<cmd>!odin build .<cr>')
-vim.keymap.set('n', '<c-t>', '<cmd>!odin test .<cr>')
+vim.keymap.set('n', '<c-t>', '<cmd>!odin check .<cr>')
 vim.keymap.set('n', '<c-r>', '<cmd>!odin run .<cr>')
